18948b5b57911bfbbfd214a9d64d7b23c0588306
msdevops13 <msdevops1305@gmail.com>
2025-04-18 22:55:40 +0530

    Add files via upload
    simple calculator

---
+++ simple_calculator/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+
+
+public class App {
+	public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("5 - 3 = " + calc.subtract(5, 3));
+    }
+}
